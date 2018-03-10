70801759dc613e4df33388e5c2cb737c2d1bf0b0
Alessandro Desantis <desa.alessandro@gmail.com>
2018-03-10 13:05:40 +0100

    Fix coding style issues

--- lib/generators/hertz/email/install_generator.rb
+++ lib/generators/hertz/email/install_generator.rb
@@ -3,7 +3,7 @@
 module Hertz
   module Email
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('templates', __dir__)
 
       def copy_initializer_file
         copy_file 'initializer.rb', 'config/initializers/hertz_email.rb'
